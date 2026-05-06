1811d3b180f7aeefeeeae3b9db3e1c7fe2faf7ab
Itanna Alakija <ialakija@icloud.com>
2026-05-06 12:07:55 +0100

    fixed error in routes

--- routes/fruits.js
+++ routes/fruits.js
@@ -6,6 +6,6 @@ router.get('/', fruits.index)
 router.get('/:name', fruits.show)
 router.post('/', fruits.create)
 router.patch('/:name', fruits.update)
-router.delete('/:name', fruits.show)
+router.delete('/:name', fruits.destroy)
 
 module.exports = router
